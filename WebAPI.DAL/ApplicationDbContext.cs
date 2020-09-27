1768f8611815421f0935483d6d74efa359ded4d9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-09-27 23:32:06 +0200

    Updated Startup.cs, updated project references;

---
+++ WebAPI.DAL/ApplicationDbContext.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Updater.Models;
+using WebAPI.Models;
+
+namespace WebAPI.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<LogModel> UpdatingLogs { get; set; }
+        public DbSet<CompanyModel> Companies { get; set; }
+        public DbSet<EmailModel> Emails { get; set; }
+        public DbSet<VesselModel> Vessels { get; set; }
+        public DbSet<AppSettings> Settings { get; set; }
+        public DbSet<SeaModel> Seas { get; set; }
+
+        //credits: https://stackoverflow.com/a/53703521
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //one-to-many
+
+            modelBuilder.Entity<CompanyModel>()
+               .HasMany(c => c.VesselOwner)
+               .WithOne(e => e.CompanyOwner);
+
+            modelBuilder.Entity<CompanyModel>()
+               .HasMany(c => c.VesselManager)
+               .WithOne(e => e.CompanyManager);
+            modelBuilder.Entity<CompanyModel>()
+               .HasMany(c => c.EmailList)
+               .WithOne(e => e.Company);
+
+            //string[] array (EF Core 2.x)
+            modelBuilder.Entity<VesselModel>()
+             .Property(e => e.PreviousOwners)
+             .HasConversion(
+                 v => string.Join(",", v),
+                  v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<VesselModel>()
+            .Property(e => e.PreviousManagers)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<VesselModel>()
+            .Property(e => e.DetailedType)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<CompanyModel>()
+            .Property(e => e.FleetTypes)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselStatus)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselType)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselFlag)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselClass)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselBuilders)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselRegion)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselAisStatus)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.VesselDetailedType)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.CompanyCountry)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<AppSettings>()
+            .Property(e => e.CompanyFleetTypes)
+            .HasConversion(
+                v => string.Join(",", v),
+                 v => v.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
