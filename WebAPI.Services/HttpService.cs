ab8f49d190d692adc3d13b539079aacd6243d764
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-10-01 17:38:19 +0200

    Corrected bug in HttpService;

--- WebAPI.Services/HttpService.cs
+++ WebAPI.Services/HttpService.cs
@@ -6,25 +6,6 @@ namespace WebAPI.Services
     {
         private static HttpClient _httpClient;
 
-        private HttpClient GetHttpClient()
-        {
-            if (_httpClient != null)
-            {
-                return _httpClient;
-            }
-
-            return CreateNewHttpClient();
-        }
-
-        private static HttpClient CreateNewHttpClient()
-        {
-            HttpClientHandler handler = new HttpClientHandler();
-            HttpClient client = new HttpClient(handler);
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3703.0 Safari/537.36");
-
-            return client;
-        }
-
         public string GetHtmlDocument(string url)
         {
             string html = string.Empty;
@@ -38,6 +19,25 @@ namespace WebAPI.Services
             }
 
             return html;
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            if (_httpClient != null)
+            {
+                return _httpClient;
+            }
+
+            return CreateNewHttpClient();
+        }
+
+        private static HttpClient CreateNewHttpClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            _httpClient = new HttpClient(handler);
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3703.0 Safari/537.36");
+
+            return _httpClient;
         }
     }
 }
