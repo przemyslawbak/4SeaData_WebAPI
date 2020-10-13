92b3af442fe7551bd9635d8a4d14187f07e1152e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-10-13 17:09:03 +0200

    Using proxy settings for HttpClient;

--- WebAPI.Services/HttpClientProvider.cs
+++ WebAPI.Services/HttpClientProvider.cs
@@ -1,30 +1,51 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 
 namespace WebAPI.Services
 {
     public class HttpClientProvider : IHttpClientProvider
     {
-        private static HttpClient _httpClient = new HttpClient();
+        private static HttpClient _client;
 
         public HttpClientProvider()
         {
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/81.0");
-            _httpClient.Timeout = new TimeSpan(0, 0, 5);
+            var proxy = new WebProxy
+            {
+                Address = new Uri($"http://{"185.30.232.123"}:{"80"}"),
+                BypassProxyOnLocal = false,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(userName: "xzjoaonj-dest", password: "cci4lgnbjke8")
+            };
+            HttpClientHandler handler = new HttpClientHandler() { Proxy = proxy };
+            _client = new HttpClient(handler);
+            _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/81.0");
         }
 
         public string GetHtmlDocument(string url)
         {
             string html = string.Empty;
 
-            HttpResponseMessage response = _httpClient.GetAsync(url).Result;
-
-            using (HttpContent content = response.Content)
+            using (HttpResponseMessage response = _client.GetAsync(url).Result)
             {
-                html = content.ReadAsStringAsync().Result;
-            }
+                using (HttpContent content = response.Content)
+                {
+                    html = content.ReadAsStringAsync().Result;
+                }
 
-            return html;
+                return html;
+            }
+        }
+
+        public string GetHtmlDocument2(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/81.0";
+                string html = client.DownloadString(url);
+
+                return html;
+            }
         }
     }
 }
